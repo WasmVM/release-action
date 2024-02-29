027bf6160adeb1177c56869af495f59a5fed6327
LuisHsu <luishsuplus@gmail.com>
2024-02-29 10:17:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let Core = require('@actions/core');
-let Github = require('@actions/github');
-let Path = require("path")
-let Fs = require('fs')
+const Core = require('@actions/core');
+const Github = require('@actions/github');
+const Path = require("path")
+const fs = require('fs')
 
 try {
     // Get octokit
-    let Octokit = Github.getOctokit(Core.getInput('token'));
+    const Octokit = Github.getOctokit(Core.getInput('token'));
     // Read & parse release note
-    let note_content = Fs.readFileSync(Path.resolve(Core.getInput('note')), {encoding: 'utf8'});
-    let [tag_str, tag_name] = note_content.match(/^\`(.*)\`\n/);
+    let note_content = fs.readFileSync(Path.resolve(Core.getInput('note')), {encoding: 'utf8'});
+    const [tag_str, tag_name] = note_content.match(/^\`(.*)\`\n/);
     note_content = note_content.substring(tag_str.length);
     // Get tags
-    let tags = Fs.readdirSync(Path.resolve(".git", "refs", "tags"))
+    let tags = fs.readdirSync(Path.resolve(".git", "refs", "tags"))
     console.log(tags)
+
     // Get assets
-    let asset_paths = Core.getInput('assets').split("\n").map(s => s.trim());
+    const asset_paths = Core.getInput('assets').split("\n").map(s => s.trim());
     console.log(`Assets: ${asset_paths}`);
 } catch (error) {
-    console.error(error.stack)
     Core.setFailed(error.message);
 }
